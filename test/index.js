3165dbdbbacdba5bef04767aee8f17330bc3a5b5
seunexplicit <seunxplicit@gmail.com>
2023-06-16 00:14:56 +0100

    chore: update text parsing

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 let textract = require( '../lib' );
 
-textract.fromFileWithPath( '/Users/xplicit/Documents/work/personal/ai-cv-generator-documents/cvs/Oluwadamilare Arabambi .pdf', { preserveLineBreaks: true }, ( error, text ) => {
-  console.log({ error, text });
+textract.fromFileWithPath( '/Users/xplicit/Documents/work/personal/ai-cv-generator-documents/cvs/Oluwakemi Akinola-Adedeji_Resume.pdf', { preserveLineBreaks: true }, ( error, text ) => {
+  console.log({ text: text.texts });
 });
